1d59535a8ef9a19adac1835619a091d91cc2be70
dan <dan.ahlquist@gmail.com>
2021-10-13 22:42:31 -0500

    Initial commit

---
+++ extract.py
@@ -0,0 +1,37 @@
+import sys
+from enum import Enum
+
+
+class LineType(Enum):
+    HEADING = 1
+    BODY = 2
+
+
+def get_line_type(line):
+    if line[0] == '(':
+        return LineType.BODY
+    else:
+        return LineType.HEADING
+
+
+def extract(in_file, out_file):
+    curr_entry = []
+    for line in in_file:
+        line_type = get_line_type(line)
+        if line_type == LineType.HEADING:
+            if len(curr_entry) > 0:
+                out_file.write(','.join(curr_entry) + '\n')
+            curr_entry = [line.split(sep='|')[0]]
+        elif line_type == LineType.BODY:
+            words = line.split(sep='|')
+            words_stripped = [w.strip() for w in words]
+            curr_entry += words_stripped[1:]
+    out_file.write(','.join(curr_entry) + '\n')
+
+
+if __name__ == "__main__":
+    in_file_name = sys.argv[1]
+    out_file_name = sys.argv[2]
+    in_file = open(in_file_name, 'r')
+    out_file = open(out_file_name, 'w')
+    extract(in_file, out_file)
---
+++ search.py
@@ -0,0 +1,39 @@
+import sys
+
+
+def contains(a, b):
+    if len(a) == len(b):
+        return False
+    parent = max(a, b, key=len)
+    child = min(a, b, key=len)
+    i, j = 0, 0
+    while i < len(parent) and j < len(child):
+        if parent[i] == child[j]:
+            j += 1
+        i += 1
+    return j == len(child)
+
+
+def find_joeys(line):
+    result = []
+    toks = line.split(',')
+    toks_stripped = [t.strip() for t in toks]
+    parent = toks_stripped[0]
+    for tok in toks_stripped[1:]:
+        if contains(parent, tok):
+            result.append(tok)
+    return result
+
+
+def search(in_file, out_file):
+    for line in in_file:
+        roos = find_joeys(line)
+        if roos:
+            parent = line.split(',')[0]
+            out_list = reversed(sorted(([parent] + roos), key=len))
+            out_line = ','.join(out_list) + '\n'
+            out_file.write(out_line)
+
+
+if __name__ == '__main__':
+    search(open(sys.argv[1], 'r'), open(sys.argv[2], 'w'))
